9d289721587bb6b6fc778ec4c9ed7bcc29d767f9
belajar00 <146102286+belajar00@users.noreply.github.com>
2024-05-05 08:16:14 +0800

    Add files via upload

---
+++ asistensi/0081.php
@@ -0,0 +1,7 @@
+<php 
+$host  = localhost
+$user = "submid"
+$pass = ""
+#db   = "formpendaftaran, wahyu0081, yu13020220081"
+$conn = mysql_connect ($host, $user, $pass, $db)
+?> 
---
+++ asistensi/index.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Document</title>
+  </head>
+  <body>
+      
+      <div class="container">
+          <div class="row">
+              <div class="1">
+    <div class="2">
+        <div class="3">
+            <h1>Asistensi</h1>
+        </div>
+        <div class="card-body">
+            <form action="0081.php" method="post">
+                <div class="form-group">
+                    <label for="nama" class="badge badge-primary">Nama</label>
+                    <input type="text" class="form-control" name="nama" id="nama" placeholder="Masukkan Nama Anda">
+                </div>
+                <div class="from-check4">
+                    <label class="form-check-label mr-5">
+                    <input class="form-check-input" type="radio" name="jenis_kelamin" id="" value="pria">Pria
+                </label>
+                <label class="form-check-label">
+                    <input class="form-check-input" type="radio" name="jenis_kelamin" id="" value="Wanita">Wanita
+                    
+                </label>
+            </div>
+            <div class="form-group mt-3">
+                <label for="alamat" class="badge badge-primary">Alamat</label>
+                <textarea class="form-control" name="alamat" id="alamat" rows="10"></textarea>
+                
+            </div>
+            <div class="from-group">
+                <label for="usia" class="badge badge-primary">Usia</label>
+                <input type="text" class="form-control" name="usia" id="usia" placeholder="Masukkan Usia Anda">
+            </div>
+            <button type="submit" class="btn btn-primary">Kirim</button>
+            <button type="rest" class="btn btn-primary">Batal</button>
+        </form>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+  </body>
+</html>
